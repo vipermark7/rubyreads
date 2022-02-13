2346ca67c0d928d68747fd6a5e01eface4958b5e
Shaffan Mustafa <shaffan1996@gmail.com>
2022-02-13 00:45:03 -0500

    CRUD app for books now works, added some styling to application.html.erb. should also try adding pictures for books later!

---
+++ rubyreads/db/migrate/20220213023225_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.date :date
+      t.text :summary
+
+      t.timestamps
+    end
+  end
+end
