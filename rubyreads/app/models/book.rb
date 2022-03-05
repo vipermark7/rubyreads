3569650304dd75eed20e88206da1f752e349253c
Shaffan Mustafa <shaffan1996@gmail.com>
2022-03-05 18:37:59 -0500

    commented out URL variable because there was string interpolation in a constant var

--- rubyreads/app/models/book.rb
+++ rubyreads/app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
     def search
         # TODO: may need to replace hardcoded API_KEY with variable from .env file
-        URL = "https://www.googleapis.com/books/v1/volumes?q=#{params[:search]}&maxResults=1&key=#{ENV["API_KEY"]}"
+        # URL = "https://www.googleapis.com/books/v1/volumes?q=#{params[:search]}&maxResults=1&key=#{ENV["API_KEY"]}"
     end
 
 end
