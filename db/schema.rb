de022a2a1d4c9a317a4077e3dff82642002f6923
Shaffan <shaffan1996@gmail.com>
2023-01-25 18:34:03 -0500

    adding migration related changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_055647) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_060926) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_055647) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "rating"
   end
 
 end
