42bddb67e847dc199afb7a5973b60c7c172f8ca9
Shaffan <shaffan1996@gmail.com>
2023-11-26 22:47:06 -0500

    small UI fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class HomeController
   def index
-  end
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get '/books', to: 'books#index'
 
   get '/books/:id', to: 'books#show'
+
+  get '/', action: :index, controller: 'books'
+
 end
