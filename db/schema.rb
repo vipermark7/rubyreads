47669131fdc7c9ac8ec08f5ac3f7df3abfef3536
Shaffan <shaffan1996@gmail.com>
2024-07-21 16:46:46 -0400

    add posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,48 +11,48 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_06_18_224046) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.date "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "cover_photo_url"
-    t.decimal "average_rating"
-    t.text "summary"
+  create_table 'books', force: :cascade do |t|
+    t.string 'title'
+    t.string 'author'
+    t.date 'date'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'cover_photo_url'
+    t.decimal 'average_rating'
+    t.text 'summary'
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "review_id", null: false
-    t.index ["review_id"], name: "index_comments_on_review_id"
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'review_id', null: false
+    t.index ['review_id'], name: 'index_comments_on_review_id'
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.text "body"
-    t.integer "book_id", null: false
-    t.decimal "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username"
-    t.index ["book_id"], name: "index_reviews_on_book_id"
+  create_table 'reviews', force: :cascade do |t|
+    t.text 'body'
+    t.integer 'book_id', null: false
+    t.decimal 'rating'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'username'
+    t.index ['book_id'], name: 'index_reviews_on_book_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "comments", "reviews"
-  add_foreign_key "reviews", "books"
+  add_foreign_key 'comments', 'reviews'
+  add_foreign_key 'reviews', 'books'
 end
